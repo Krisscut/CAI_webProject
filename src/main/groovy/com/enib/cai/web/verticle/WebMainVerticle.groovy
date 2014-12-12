e2ec6ab84b1d8957ec7b55f6f00dd5fd5244ec68
Krisscut <krisscut@hotmail.fr>
2014-12-12 18:31:22 +0100

    Clean webServer
    Clean webServer

--- src/main/groovy/com/enib/cai/web/verticle/WebMainVerticle.groovy
+++ src/main/groovy/com/enib/cai/web/verticle/WebMainVerticle.groovy
@@ -18,71 +18,8 @@ class WebMainVerticle extends Verticle
 
         println "Launch the verticle routeMatcher"
 
-        /*
-   RouteMatcher routeMatcher = new RouteMatcher()
-
-
-   // API Route Matcher send to the event bus
-   routeMatcher.get("/api/beers/:id") { req ->
-     String id = req.params.get("id")
-     // send the message throw the eventbus
-     eb.send("beers.service", id) { response ->
-       // get the response from the eventbus and send it as response
-       println("I received a reply before the timeout of 5 seconds");
-       req.response.end(response.body)
-     }
-   }
-
-
-   // API Route Matcher send to the event bus
-   routeMatcher.get("/img/:name") { req  ->
-     String filename = req.get("name")
-
-     eb.send("images.service", filename) { response ->
-       req.response.putHeader("Content-Length", Integer.toString(response.body.length))
-       req.response << response.body()
-       req.response.end();
-     }
-   }
-
-   // otherwise  send file
-   routeMatcher.noMatch() { req ->
-     String file = "";
-     if (req.path.equals("/")) {
-       file = "index.html";
-     } else if (!req.path.contains("..")) {
-       file = req.path();
-     }
-     req.response.sendFile("web/" + file);
-   }
-   */
-        //def server = vertx.createHttpServer();
-        //server.requestHandler{ request -> println "A request has arrived on the server!" }.listen(8080, "localhost")
-        //vertx.createHttpServer().requestHandler(routeMatcher.asClosure()).listen(44081)
-
-
-        /*
-          def server = vertx.createHttpServer()
-          def routeMatcher = new RouteMatcher()
-
-          routeMatcher.noMatch
-                  {
-              req -> req.response.end "Nothing matched"
-          }
-          */
-
-        /*
-          server.requestHandler { req ->
-              req.response.end "<html><body><h1>Hello from vert.x!</h1></body></html>"
-          }.listen(44081, "localhost");
-          */
-
-        //server.requestHandler {routeMatcher.asClosure()}.listen(44081, "localhost");
-
         def rm = new RouteMatcher()
-
-
-
+        
         // Extract the params from the uri
 
         // API Route Matcher send to the event bus
@@ -121,33 +58,11 @@ class WebMainVerticle extends Verticle
             }
         }
 
-        /*
-        // API Route Matcher send to the event bus
-        rm.get("/img/:name") { req  ->
-            String filename = req.get("name")
-
-            eb.send("images.service", filename) { response ->
-                req.response.putHeader("Content-Length", Integer.toString(response.body.length))
-                req.response << response.body()
-                req.response.end();
-            }
-        }
-        */
-
         // otherwise  send file
         rm.noMatch() { req ->
             def file = req.uri == "/" ? "index.html" : req.uri
             req.response.sendFile "web/$file"
         }
-
-// Catch all - serve the index page
-
-        /*
-        rm.getWithRegEx('.*') { req ->
-
-            req.response.sendFile "route_match/index.html"
-        }
-          */
 
         vertx.createHttpServer().requestHandler(rm.asClosure()).listen(8080)
 
