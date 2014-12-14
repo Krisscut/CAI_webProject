ef18aa265d4f1fe8002ec66219e7922c91a39c61
Lastcall310 <ivers2@hotmail.com>
2014-12-14 21:46:25 +0100

    amelioration

--- src/main/resources/web/js/app.js
+++ src/main/resources/web/js/app.js
@@ -3,25 +3,71 @@
 /* App Module */
 
 var wormBuyer = angular.module('WormBuyer', [
-  'ngRoute',
+  'ngRoute'/*,
   'WormsControllers',
   'WormFilters',
   'WormServices',
-  'WormAnimations'
+  'WormAnimations'*/
 ]);
 
-angularBeer.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/beers', {
-        templateUrl: 'partials/beer-list.html',
-        controller: 'BeerListCtrl'
-      }).
-      when('/beers/:beerId', {
-        templateUrl: 'partials/beer-detail.html',
-        controller: 'BeerDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/beers'
-      });
-  }]);
+
+wormBuyer.controller("wormtruc", ["$scope", "$http", function($scope,$http){
+	$scope.worms = [];
+	$scope.equipments =[];
+	$scope.panier =[];
+	
+	$scope.addTo = function(object){
+	$scope.panier.push(object);
+	
+	}
+	
+	$scope.getWormData = function(){
+		$http.get("/api/worms/getAll").
+		success(function(data, status){
+			$scope.worms = data.result;
+			$scope.equipments = [];
+			$scope.bonuses = [];
+		}).
+		error(
+		function(data, status){
+			alert("erreur!")
+		}
+		);
+	}
+	
+	$scope.getEquipmentData = function(){
+		$http.get("/api/equipments/getAll").
+		success(function(data, status){
+		if (data.status !="Unknow Command"){
+			$scope.equipments = data.result;
+			$scope.worms = [];
+			$scope.bonuses = [];
+		}else{
+			alert(data.status);
+		}
+		}).
+		error(
+		function(data, status){
+			alert("erreur!")
+		}
+		);
+	}
+		$scope.getBonusData = function(){
+		$http.get("/api/bonuses/getAll").
+		success(function(data, status){
+		if (data.status !="Unknow Command"){
+			$scope.bonuses = data.result;
+			$scope.worms = [];
+			$scope.equipments = [];
+		}else{
+			alert(data.status);
+		}
+		}).
+		error(
+		function(data, status){
+			alert("erreur!")
+		}
+		);
+	}
+	
+}]);
